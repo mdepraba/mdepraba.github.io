a50ae8ab8f0e96ac595e9e813499709c45aa6851
mdepraba <128952605+mdepraba@users.noreply.github.com>
2024-01-25 20:20:21 +0700

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+   theme: {
+     extend: {
+       colors: {
+         slate: {
+           1000: '#161616',
+         },
+       },
+       backgroundImage: {
+         hero: "url('src/assets/drakaniabg.png')"
+       }
+     },
+   },
+ }          
